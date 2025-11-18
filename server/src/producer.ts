8dfb3c35c982d88f0bc799d248cf111cdeb3b06e
Rishabh Tyagi <rishabh.tyagi2311@gmail.com>
2025-11-19 00:42:42 +0530

    producer/consumer test code

---
+++ server/src/producer.ts
@@ -0,0 +1,36 @@
+import amqplib from "amqplib"
+
+
+const amqp  = require('amqplib')
+
+
+export async function sendMessage()
+{
+    try{
+        const connection = await amqp.connect('amqp://localhost')
+        const channel = await connection.createChannel();
+
+        const queue = "test_queue"
+        await channel.assertQeue(queue , {durable: true});
+
+
+        const message = 'hello from node js'
+        channel.sendToQueue(queue, Buffer.from(message) , {persistent : true})
+
+
+        console.log("message sent");
+
+        setTimeout(() => {
+            channel.close();
+            connection.close();
+            process.exit(0);
+
+        },500 )
+        
+
+    }
+    catch(error)
+    {
+        console.error('Error : ', error)
+    }   
+}
---
+++ worker/index.py
@@ -0,0 +1,28 @@
+import pika 
+import json 
+
+def callback(ch, method, properties, body):
+    """called when a message arrives"""
+    message = body.devode('utf-8')
+    print(f"recieved : {message}")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+
+def consume_messages():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+
+    queue = 'test_queue'
+    channel.queue_declare(queue=queue, durable=True)
+
+    channel.basic_consume(queue=queue, on_message_callback = callback)
+    print(f'waiting for messafes from "{queue}" ....')
+
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    consume_messages()
+
+
